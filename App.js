a113fe981b2a743edf0be8966718d4b7d5bc4db5
lythanhhai <lythanhhait@gmail.com>
2022-07-17 16:27:25 +0700

    learn react-native

--- App.js
+++ App.js
@@ -1,18 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Welcome from './Component/Welcome';
 
 export default function App() {
+  const names = ["Ryan", "Zen", "David"]
+  const element = names.map((name, index) => {
+    return(
+      <Welcome name={name} key={index} />
+    )
+  })
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      {/* <Welcome name="Ryan" />
+      <Welcome name="Zen" />
+      <Welcome name="David" /> */}
+      
+      {element}
+      
     </View>
   );
 }
 
+
+// top left
+
+// center left
+
+// bottom left
+
+// top right
+
+// center right
+
+// bottom right
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1,  
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
