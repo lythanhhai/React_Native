c32284f23c388491644d6952b88073c1bee65600
lythanhhai <lythanhhait@gmail.com>
2022-09-07 12:02:54 +0700

    add ui login and save and get a user

--- chatapp/App.js
+++ chatapp/App.js
@@ -5,6 +5,7 @@ import Home from "./screens/Home/Home";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Register from "./screens/Register";
+import Login from "./screens/Login";
 
 const Stack = createStackNavigator();
 
@@ -16,6 +17,7 @@ export default function App() {
           headerShown: false,
         }}
       >
+        <Stack.Screen name={"Login"} component={Login} />
         <Stack.Screen name={"Register"} component={Register} />
         <Stack.Screen name={"Home"} component={Home} />
         <Stack.Screen name={"Detail"} component={DetailChat} />
--- chatapp/screens/Home/Home.js
+++ chatapp/screens/Home/Home.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../components/Header/Header";
 import Unread from "../../components/Unread/Unread";
 import ChatItem from "../../components/ChatItem/ChatItem";
 import ListChat from "../../components/ListChat/ListChat";
 import { View, StyleSheet } from "react-native";
+
+import {
+  auth, onAuthStateChanged, firebaseDatabaseRef, firebaseSet, firebaseDatabase
+}
+from "../../firebase/firebase"
 
 const styles = StyleSheet.create({
   container: {
@@ -16,6 +21,23 @@ const styles = StyleSheet.create({
 });
 
 const Home = ({ navigation }) => {
+  // useEffect(() => {
+  //   onAuthStateChanged(auth, (user) => {
+  //     if(user)
+  //     {
+  //       const userId = user.uid
+  //       firebaseSet(firebaseDatabaseRef(
+  //         firebaseDatabase,
+  //         `users/${userId}`
+  //       ), {
+  //         email: user.email,
+  //         emailVerified: user.emailVerified,
+  //         accessToken: user.accessToken,
+  //       })
+        
+  //     }
+  //   })
+  // })
   return (
     <>
       <Header title="Notification" pages={"Home"} />
